0a84f4a266379f4d072906184b3a6653019b3273
rosg150498 <rosg150498@gmail.com>
2023-02-28 22:30:01 +0000

    Change margin top to qu h2 and add horizontal rule to question div to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,17 +17,22 @@
 
     // Adding the question text to the <div> with style
     const questionText = document.createElement('p');
-    questionText.textContent = `${index + 1}. ${question.question}`;
+    questionText.textContent = `Question ${index + 1} ) ${question.question}`;
     questionText.style.fontSize = '24px';
     questionText.style.color = '#FFFFFF';
     questionText.style.fontWeight = 'bold';
-    questionText.style.marginTop = '-22rem';
-    questionText.style.marginLeft = '20rem';
+    questionText.style.marginTop = '-26rem';
+    questionText.style.marginLeft = '12rem';
     questionText.setAttribute('class', 'question-text');
     questionElement.appendChild(questionText);
-    
 
-    // Adding the question to the quiz container
+    // Adding a horizontal rule to the <div>
+   const hrElement = document.createElement('hr');
+   hrElement.style.width = `${question.question.length}ch`;
+   hrElement.style.marginLeft = '12rem';
+   questionElement.appendChild(hrElement);
+   
+   // Adding the question to the quiz container
     quizElement.appendChild(questionElement);
   });
 }
